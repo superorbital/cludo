800e335ff1ad0384c5b683e54b1460ff419d2d0c
Robert Chu <allegrormc@gmail.com>
2021-05-05 09:28:27 -0700

    Generate server main and cli stub.

--- gen-swagger.sh
+++ gen-swagger.sh
@@ -6,7 +6,8 @@ docker run --rm -it \
     -v $HOME:$HOME \
     -w $(pwd) \
     quay.io/goswagger/swagger \
-    generate server -f ./swagger.yaml --exclude-main
+    generate server -f ./swagger.yaml --main-package=server
+    # --exclude-main
 
 docker run --rm -it \
     --user $(id -u):$(id -g) \
@@ -15,3 +16,11 @@ docker run --rm -it \
     -w $(pwd) \
     quay.io/goswagger/swagger \
     generate client -f ./swagger.yaml
+
+docker run --rm -it \
+    --user $(id -u):$(id -g) \
+    -e GOPATH=$HOME/go:/go \
+    -v $HOME:$HOME \
+    -w $(pwd) \
+    quay.io/goswagger/swagger \
+    generate cli -f ./swagger.yaml
