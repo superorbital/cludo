db8813de8d48d1511f1ff1db89de9b5fc53e54ce
Sean P. Kane <sean@superorbital.io>
2021-05-10 17:38:37 -0700

    Updating tests for cludo exec

--- cludo/exec_operation.go
+++ cludo/exec_operation.go
@@ -22,9 +22,14 @@ import (
 func runOperationExec(cmd *cobra.Command, args []string, conf config.Config) string {
 
 	cc := conf.Client["default"]
+
+	if debug {
+		fmt.Printf("\n%#v\n", cc)
+	}
+
 	u, err := url.Parse(cc.ServerURL)
 	if err != nil {
-		fmt.Printf("[ERROR] Unable to parse server url. (%s)", err)
+		return fmt.Sprintf("[ERROR] Unable to parse server url. (%s)", err)
 	}
 	hostport := u.Host
 	apipath := u.Path
@@ -39,50 +44,37 @@ func runOperationExec(cmd *cobra.Command, args []string, conf config.Config) str
 
 	keyPath, err := homedir.Expand(cc.KeyPath)
 	if err != nil {
-		fmt.Printf("[ERROR] Could not expand SSH Private Key path. (%s)", err)
-		return ""
+		return fmt.Sprintf("[ERROR] Could not expand SSH Private Key path. (%s)", err)
 	}
 
 	rawKey, err := ioutil.ReadFile(keyPath)
 	if err != nil {
-		fmt.Printf("[ERROR] Reading SSH Private Key. (%s)", err)
-		return ""
+		return fmt.Sprintf("[ERROR] Reading SSH Private Key. (%s)", err)
 	}
 
 	key, err := auth.DecodePrivateKey(rawKey, []byte(cc.Passphrase))
 	if err != nil {
-		fmt.Printf("[ERROR] Decoding SSH Private Key. (%s)", err)
-		return ""
+		return fmt.Sprintf("[ERROR] Decoding SSH Private Key. (%s)", err)
 	}
 
 	signer := auth.NewDefaultSigner(key)
 
-	// Let's make sure we can talk to the server now
-	_, err = client.Environment.GenerateEnvironment(params, signer.CludoAuth())
-	if err != nil {
-		fmt.Printf("[ERROR] Unable to generate environment. (%s)", err)
-		return ""
-	}
-
-	//return client, nil
-
-	if debug {
-		fmt.Printf("\n%#v\n", cc)
-	}
-
 	if dryRun {
-
 		logDebugf("dry-run flag specified. Skip sending request.")
 		return ""
 	}
 
-	// make request and then print result
-	msgStr, err := "\nexec ran!\nserver_url: "+cc.ServerURL+"\nkey_path  : "+cc.KeyPath+"\ncommand   : "+string(strings.Join(args, " ")), *new(error)
-	//msgStr, err := "\nexec ran!\nserver_url: "+conf["server_url"]+"\nkey_path  : "+conf["key_path"]+"\ncommand   : "+string(strings.Join(args, " ")), *new(error)
-
+	// Let's make sure we can talk to the server now
+	_, err = client.Environment.GenerateEnvironment(params, signer.CludoAuth())
 	if err != nil {
-		return ""
+		//return fmt.Sprintf("[ERROR] Unable to generate environment. (%s)", err)
 	}
+
+	// At the moment seeing this error from the above code:
+	// [ERROR] Unable to generate environment. (Post "http://127.0.0.1:8080/environment": net/http: invalid header field value "\x92\u007f\xbd\xb3H6)B\nH:mtVduNdGxp6gh96ULfHVPKnA9DAIeJ6QPkk3eU4/WDR7APrEqrt0w8Ey4QcPMQzSXeXR0Hixpzd9U6ruC9OIHbyawQDS3lm+ho0X1pFeHmGNQSGJkFitsyYjbV42y0wrkbXaww9LhYgt7OCKg0wLRAm2ezANuKwVLJDYGeQnON6BukMEfSfUvQElAnCmbXfyj6Qet+pKKxUV6aifjX/JYiAV9IK3+A3HmiiVQCAIyNfZxiCr7NTSR3OqEIxtH/upsK1Lrvmtz9h/ug9OVYeDXgOU1Vd+PqaTAzoPg3C1B/zBqnqMtG/Ucw3KPhXTe9aIp++cfPHiJESgTgA7y+xq9A==" for key X-Cludo-Key)
+
+	// make request and then print result
+	msgStr := "\nexec ran!\nserver_url: " + cc.ServerURL + "\nshell_path: " + cc.ShellPath + "\ncommand   : " + string(strings.Join(args, " "))
 
 	return msgStr
 }
---
+++ data/README.md
@@ -0,0 +1,6 @@
+# Test Data
+
+## SSH Keys
+
+* The passphrase for `id_rsa_TEST_KEY_PP` is *cludo*
+
