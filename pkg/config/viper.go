6a4a4bfee4149a747d09b232d37b21bd230f6470
Will Pragnell <will.pragnell@superorbital.io>
2022-02-10 11:15:05 -0800

    Fix custom config paths for client
    Fixes #108

--- pkg/config/viper.go
+++ pkg/config/viper.go
@@ -69,7 +69,6 @@ func ConfigureViper(executable string, configFile string) error {
 	// local repo cludo.yaml file
 	// Only check for this when we are using the client.
 	if executable == "cludo" {
-		viper.SetConfigName("cludo")
 		viper.AddConfigPath(".")
 		if err := viper.MergeInConfig(); err != nil {
 			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
