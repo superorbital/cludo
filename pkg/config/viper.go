106a86e20e3ff67a6a35545c3c13fa9982561b6c
Will Pragnell <will.pragnell@superorbital.io>
2022-02-10 11:13:27 -0800

    Stop exporting unnecessary stuff from pkg/config

--- pkg/config/viper.go
+++ pkg/config/viper.go
@@ -12,39 +12,39 @@ import (
 const CludodExecutable = "cludod"
 const CludoExecutable = "cludo"
 
-// EnvPrefix defines a prefix required on all environment variables bound to Config.
+// envPrefix defines a prefix required on all environment variables bound to Config.
 // For example the path "number" is bound to CLUDO_NUMBER.
-const EnvPrefix = "CLUDO"
+const envPrefix = "CLUDO"
 
-var ErrConfigNotFound = errors.New("Failed to load configuration file: File not found")
+var errConfigNotFound = errors.New("Failed to load configuration file: File not found")
 
-func ErrConfigLoadFailed(cause error) error {
+func errConfigLoadFailed(cause error) error {
 	return fmt.Errorf("Failed to load configuration file: %v", cause)
 }
 
-func ErrHomeDirFailed(cause error) error {
+func errHomeDirFailed(cause error) error {
 	return fmt.Errorf("Failed to get user home directory: %v", cause)
 }
 
-func ErrWorkingDirFailed(cause error) error {
+func errWorkingDirFailed(cause error) error {
 	return fmt.Errorf("Failed to get working directory: %v", cause)
 }
 
 func ConfigureViper(executable string, configFile string) error {
 	cwd, err := os.Getwd()
 	if err != nil {
-		return ErrWorkingDirFailed(err)
+		return errWorkingDirFailed(err)
 	}
 	homedir, err := os.UserHomeDir()
 	if err != nil {
-		return ErrHomeDirFailed(err)
+		return errHomeDirFailed(err)
 	}
 
 	// When we bind flags to environment variables expect that the
 	// environment variables are prefixed, e.g. a flag like --profile
 	// binds to an environment variable CLUDO_PROFILE. This helps
 	// avoid conflicts.
-	viper.SetEnvPrefix(EnvPrefix)
+	viper.SetEnvPrefix(envPrefix)
 	viper.AutomaticEnv()
 
 	// Read configuration
@@ -61,9 +61,9 @@ func ConfigureViper(executable string, configFile string) error {
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			// Config file not found; ignore error if desired
-			return ErrConfigNotFound
+			return errConfigNotFound
 		} else {
-			return ErrConfigLoadFailed(err)
+			return errConfigLoadFailed(err)
 		}
 	}
 	// local repo cludo.yaml file
@@ -73,9 +73,9 @@ func ConfigureViper(executable string, configFile string) error {
 		viper.AddConfigPath(".")
 		if err := viper.MergeInConfig(); err != nil {
 			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-				return ErrConfigNotFound
+				return errConfigNotFound
 			} else {
-				return ErrConfigLoadFailed(err)
+				return errConfigLoadFailed(err)
 			}
 		}
 	}
